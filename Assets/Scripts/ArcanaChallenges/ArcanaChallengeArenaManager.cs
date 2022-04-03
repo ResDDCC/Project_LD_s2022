9b19925ef7fecd342dbe5d17c7ab8955e84cbf3e
reverienest <reverienest@gmail.com>
2022-04-03 17:36:20 -0400

    Revert "Idle Anims Spliced, Minor Data Changes"
    This reverts commit 9c47bdccf6b3168f0b4c20ac8746cd2baf44fc4c.

--- Assets/Scripts/ArcanaChallenges/ArcanaChallengeArenaManager.cs
+++ Assets/Scripts/ArcanaChallenges/ArcanaChallengeArenaManager.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class ArcanaChallengeArenaManager : MonoBehaviour
 {
-
-    [SerializeField]
-    protected ArcanaChallengeData challengeData;
-
-    [SerializeField] 
-    protected List<List<GameObject>> waves = new List<List<GameObject>>();
-
-    [SerializeField] 
-    protected int waveCount;
-
-    [SerializeField] 
-    protected float timeProvided;
-
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/ArcanaChallenges/StrengthChallengeManager.cs
+++ Assets/Scripts/ArcanaChallenges/StrengthChallengeManager.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class StrengthChallengeManager : ArcanaChallengeArenaManager
 {
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Begin Strength Challenge");
-
-        this.waveCount = 0;
-        //this.timeProvided = StrengthChallengeData.timeProvided;
+        
     }
 
     // Update is called once per frame
