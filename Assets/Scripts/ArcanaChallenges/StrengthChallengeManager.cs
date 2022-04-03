9c47bdccf6b3168f0b4c20ac8746cd2baf44fc4c
reverienest <reverienest@gmail.com>
2022-04-03 04:28:54 -0400

    Idle Anims Spliced, Minor Data Changes

--- Assets/Scripts/ArcanaChallenges/StrengthChallengeManager.cs
+++ Assets/Scripts/ArcanaChallenges/StrengthChallengeManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class StrengthChallengeManager : ArcanaChallengeArenaManager
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Begin Strength Challenge");
+
+        this.waveCount = 0;
+        //this.timeProvided = StrengthChallengeData.timeProvided;
     }
 
     // Update is called once per frame
